d94b65a78831a4bcdda02e6f17284c1d01406fc8
vinhngph <136446610+vinhngph@users.noreply.github.com>
2024-12-30 19:11:25 +0700

    fix: loading time

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,94 @@
+function getBaseDomain(hostname) {
+    const parts = hostname.split('.');
+
+    if (parts.length <= 2) {
+        return hostname;
+    }
+
+    const topLevelDomain = parts.slice(-2).join('.');
+    return topLevelDomain;
+}
+
+function generateDocument() {
+    function docView() {
+        const head = document.querySelector('link[rel="stylesheet"][href*="doc-assets.studocu.com"]').outerHTML;
+        const tit = document.getElementsByTagName("h1")[0].innerHTML;
+
+        let content = document.getElementById('page-container');
+        content.querySelectorAll('[class*="banner-wrapper"]').forEach(element => {
+            element.remove();
+        })
+        let pages = content.childNodes;
+
+        for (i = 0; i < pages.length; i++) {
+            pages[i].childNodes[0].style = "display: block;";
+        }
+
+        const pdf = pages[0].parentNode.parentNode.parentNode.innerHTML;
+
+        const width = pages[0].offsetWidth;
+        const height = pages[0].offsetHeight;
+
+        if (width > height) {
+            print_opt = "{@page { size: A5 landscape; margin: 0; }}";
+        } else {
+            print_opt = "{@page { size: A5 portrait; margin: 0; }}";
+        }
+
+        let newWindow = window.open("", "_blank");
+        newWindow.document.getElementsByTagName("head")[0].innerHTML = head + "<style> .nofilter{filter: none !important;} </style>" + "<style> @media print " + print_opt + "</style>";
+        newWindow.document.title = tit;
+        newWindow.document.getElementsByTagName("body")[0].innerHTML = pdf;
+        newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
+        newWindow.document.close();
+
+        setTimeout(() => {
+            const e = newWindow.document.getElementById("page-container");
+            if (e) {
+                e.scrollTo({
+                    top: e.scrollHeight,
+                    behavior: "smooth",
+                });
+            }
+
+            setTimeout(() => {
+                newWindow.print();
+            }, 1500);
+        }, 1000);
+    }
+
+    const documentWrapper = document.getElementById("document-wrapper");
+    if (documentWrapper) {
+        documentWrapper.scrollTo({
+            top: documentWrapper.scrollHeight,
+            behavior: "smooth",
+        });
+    }
+
+    setTimeout(function () {
+        docView();
+    }, 5000);
+}
+
+async function deleteDomainCookies(domain) {
+    try {
+        const cookies = await chrome.cookies.getAll({ domain });
+
+        if (cookies.length === 0) return null;
+
+        let pending = cookies.map((cookie) => {
+            chrome.cookies.remove({
+                url: `http${cookie.secure ? 's' : ''}://${cookie.domain}${cookie.path}`,
+                name: cookie.name,
+                storeId: cookie.storeId
+            })
+        })
+        await Promise.all(pending);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 document.getElementById("btn-refresh").addEventListener("click", async () => {
     try {
         const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -5,9 +96,9 @@ document.getElementById("btn-refresh").addEventListener("click", async () => {
         if (!tab && !tab.url) return;
 
         const url = new URL(tab.url);
-        const domain = url.hostname;
+        const domain = getBaseDomain(url.hostname);
 
-        const allowedDomains = ["www.studocu.com", "www.studocu.vn"];
+        const allowedDomains = ["studocu.com", "studocu.vn"];
         if (!allowedDomains.includes(domain)) return;
 
         chrome.scripting.executeScript(
@@ -19,7 +110,9 @@ document.getElementById("btn-refresh").addEventListener("click", async () => {
                 const flag = results[0]?.result;
                 if (flag) {
                     if (deleteDomainCookies(domain)) {
-                        chrome.tabs.reload(tab.id);
+                        setTimeout(() => {
+                            chrome.tabs.reload(tab.id);
+                        }, 1000);
                     } else {
                         alert("There are some errors!");
                     }
@@ -55,7 +148,10 @@ document.getElementById("btn-download").addEventListener("click", async () => {
                 if (flag) {
                     alert("Please use \"Refresh\" function to clear all banners!");
                 } else {
-                    chrome.runtime.sendMessage({ action: "download_content", tab })
+                    chrome.scripting.executeScript({
+                        target: { tabId: tab.id },
+                        func: generateDocument
+                    })
                 }
             }
         )
@@ -63,22 +159,3 @@ document.getElementById("btn-download").addEventListener("click", async () => {
         console.error(error);
     }
 })
-
-async function deleteDomainCookies(domain) {
-    try {
-        const cookies = await chrome.cookies.getAll({ domain });
-
-        if (cookies.length === 0) return null;
-
-        let pending = cookies.map((cookie) => {
-            chrome.cookies.remove({
-                url: `http${cookie.secure ? 's' : ''}://${cookie.domain}${cookie.path}`,
-                name: cookie.name,
-                storeId: cookie.storeId
-            })
-        })
-        await Promise.all(pending);
-    } catch (error) {
-        console.error(error);
-    }
-}
