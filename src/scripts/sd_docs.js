963db486f5f687c1d29206abe4d3ce20f23fd9aa
vinhngph <136446610+vinhngph@users.noreply.github.com>
2025-01-11 21:24:05 +0700

    fix: tab.url

--- src/scripts/sd_docs.js
+++ src/scripts/sd_docs.js
@@ -1,5 +1,7 @@
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete" && tab.url.includes("www.studocu.")) {
+    if (changeInfo.status === "complete" && tab.url) {
+        if (!tab.url.includes("www.studocu.")) return;
+
         const origin = new URL(tab.url).origin;
 
         chrome.cookies.get({ name: "sd_docs", url: origin }, (cookie) => {
