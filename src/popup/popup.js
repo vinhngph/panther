246589321c3200f4354f99b537461cbdf18d8309
vinhngph <136446610+vinhngph@users.noreply.github.com>
2025-04-07 00:14:27 +0700

    refactor: separate modules in main()

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,229 +1,240 @@
-async function genDoc() {
-    if (!document.querySelector("._95f5f1767857")) {
-        const genPDF = () => {
-            const docSize = document.getElementById("page-container-wrapper")?.childNodes[0].childNodes[0];
-            if (!docSize) return;
-
-            const pxToMm = (px) => {
-                const div = document.createElement("div");
-                div.style.width = "100mm";
-                div.style.position = "absolute";
-                div.style.visibility = "hidden";
-                document.body.appendChild(div);
-
-                const mmPerPx = 100 / div.getBoundingClientRect().width;
-                document.body.removeChild(div);
-
-                return px * mmPerPx;
-            }
-
-            const printConfig = (width, height) => {
-                width = pxToMm(width);
-                height = pxToMm(height);
-
-                if (width < height) {
-                    const scaleX = 210 / width;
-                    const scaleY = 297 / height;
-
-                    return `{
-                        @page { 
-                            size: A4 portrait; margin: 0; 
-                        }
-                        #page-container {
-                            transform: scale(${scaleX}, ${scaleY});
-                            transform-origin: top left;
-                        }
-                    }`
-                } else {
-                    const scaleX = 297 / width;
-                    const scaleY = 210 / height;
-
-                    return `{
-                        @page { 
-                            size: A4 landscape; margin: 0; 
-                        }
-                        #page-container {
-                            transform: scale(${scaleX}, ${scaleY});
-                            transform-origin: top left;
-                        }
-                    }`
-                }
-            }
-
-            const print_opt = printConfig(docSize.clientWidth, docSize.clientHeight);
-
-            const docStyles = document.querySelector('link[rel="stylesheet"][href*="doc-assets"][href*=".studocu.com"]').outerHTML;
-
-            const doc = document.getElementById("page-container-wrapper")?.cloneNode(true);
-            if (!doc) return;
-            doc.style = null;
-            doc.querySelectorAll('[class*="banner-wrapper"]').forEach(element => {
-                if (!element) return;
-                element.remove();
-            })
-            doc.childNodes[0].childNodes.forEach((element) => {
-                element.childNodes[0].style = "display: block";
-            })
-
-            const docHead = docStyles + "<style> @media print " + print_opt + "</style>";
-            const docTitle = document.title;
-            const docBody = doc.outerHTML;
-
-            const newWindow = window.open("");
-            newWindow.document.head.innerHTML = docHead;
-            newWindow.document.title = docTitle;
-            newWindow.document.body.innerHTML = docBody;
-
-            async function download_count_down(newWindow) {
-                const notification = newWindow.document.createElement('div');
-                Object.assign(notification.style, {
-                    position: 'fixed',
-                    top: '20px',
-                    right: '20px',
-                    backgroundColor: '#FF5722',
-                    color: 'white',
-                    padding: '15px',
-                    borderRadius: '5px',
-                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-                    opacity: '0',
-                    transition: 'opacity 0.5s ease',
-                    fontSize: '20px',
-                    zIndex: '9999'
-                });
-
-                let countdown = 5;
-                notification.textContent = `Download in ${countdown}...`;
-                newWindow.document.body.appendChild(notification);
-                newWindow.setTimeout(() => notification.style.opacity = '1', 10);
-
-                await new Promise((resolve) => {
-                    const interval = newWindow.setInterval(() => {
-                        countdown--;
-                        notification.textContent = `Download in ${countdown}...`;
-                        if (countdown <= 0) {
-                            newWindow.clearInterval(interval);
-                            notification.style.opacity = '0';
-                            notification.remove();
-                            resolve();
-                        }
-                    }, 1000);
-                });
-
-                newWindow.print();
-            }
-            download_count_down(newWindow);
+async function main() {
+    // ----------------------------------------------------------
+    // Clone this document
+    // ----------------------------------------------------------
+    const cloneDoc = () => {
+        const doc = document.getElementById("page-container-wrapper")?.cloneNode(true);
+        if (!doc) {
+            alert("Error: Clone document");
+            return;
         }
 
-        const loadContent = async () => {
-            const doc = document.getElementById("document-wrapper");
-            if (!doc) return;
-            const currentPosition = doc.scrollTop;
+        // Data processing
+        doc.style = null;
+        doc.querySelectorAll('[class*="banner-wrapper"]').forEach(e => {
+            if (!e) return;
+            e.remove();
+        });
+        doc.childNodes[0].childNodes.forEach((e) => {
+            e.childNodes[0].style = "display: block";
+        });
 
-            const pageContainer = document.getElementById("page-container");
+        return doc;
+    }
+    // ----------------------------------------------------------
 
-            if (!pageContainer) {
-                console.error("Page container element not found");
+
+    // ----------------------------------------------------------
+    // Print configurations
+    // ----------------------------------------------------------
+    const printConfig = () => {
+        // Get document size
+        const docSize = document.getElementById("page-container-wrapper")?.childNodes[0].childNodes[0];
+        if (!docSize) return;
+
+        // Convert px to mm
+        const pxToMm = (px) => {
+            const div = document.createElement("div");
+            div.style.width = "100mm";
+            div.style.position = "absolute";
+            div.style.visibility = "hidden";
+            document.body.appendChild(div);
+
+            const mmPerPx = 100 / div.getBoundingClientRect().width;
+            document.body.removeChild(div);
+
+            return px * mmPerPx;
+        }
+
+        // portrait: true | landscape: false
+        const docStruct = docSize.clientWidth < docSize.clientHeight ? "portrait" : "landscape";
+
+        const docScale = [];
+        if (docStruct == "portrait") {
+            docScale.push(210 / pxToMm(docSize.clientWidth));
+            docScale.push(297 / pxToMm(docSize.clientHeight));
+        } else {
+            docScale.push(297 / pxToMm(docSize.clientWidth));
+            docScale.push(210 / pxToMm(docSize.clientHeight));
+        }
+
+
+        const css = document.createElement("style");
+        css.innerText = `@media print { @page { size: A4 ${docStruct}; margin: 0; } #page-container { transform: scale(${docScale[0]}, ${docScale[1]}); transform-origin: top left; }}`;
+        return css;
+    }
+    // ----------------------------------------------------------
+
+
+    // ----------------------------------------------------------
+    // Get current position
+    const getCurrentPosition = () => {
+        return document.getElementById("document-wrapper")?.scrollTop;
+    }
+    // ----------------------------------------------------------
+
+    // ----------------------------------------------------------
+    // Scan document
+    // ----------------------------------------------------------
+    const scanDoc = async () => {
+        const pageContainer = document.getElementById("page-container");
+        if (!pageContainer) {
+            alert("Page container element not found");
+            return;
+        }
+
+        const elements = Array.from(pageContainer.childNodes);
+        for (const element of elements) {
+            try {
+                const content = element.childNodes[0];
+                if (content?.className === "page-content" && content.childNodes.length !== 2) {
+                    element.scrollIntoView({
+                        behavior: "smooth",
+                        block: "start",
+                    });
+
+                    await new Promise((resolve) => {
+                        const observer = new MutationObserver((mutations, observerInstance) => {
+                            if (content.childNodes.length === 2 && content.childNodes[0].childNodes.length > 0) {
+                                observerInstance.disconnect();
+                                resolve();
+                            }
+                        });
+                        observer.observe(content, { childList: true, subtree: true });
+                    });
+                }
+            } catch (error) {
+                alert("Error processing element:", error);
                 return;
             }
-
-            const elements = Array.from(pageContainer.childNodes);
-            for (const element of elements) {
-                try {
-                    const content = element.childNodes[0];
-                    if (content?.className === "page-content" && content.childNodes.length !== 2) {
-                        element.scrollIntoView({
-                            behavior: "smooth",
-                            block: "start",
-                        });
-
-                        await new Promise((resolve) => {
-                            const observer = new MutationObserver((mutations, observerInstance) => {
-                                if (content.childNodes.length === 2 && content.childNodes[0].childNodes.length > 0) {
-                                    observerInstance.disconnect();
-                                    resolve();
-                                }
-                            });
-                            observer.observe(content, { childList: true, subtree: true });
-                        });
-                    }
-                } catch (error) {
-                    console.error("Error processing element:", error);
-                }
-            }
-
-            async function countdownAndScroll(doc, currentPosition) {
-                const notification = document.createElement('div');
-                Object.assign(notification.style, {
-                    position: 'fixed',
-                    top: '20px',
-                    right: '20px',
-                    backgroundColor: '#FF5722',
-                    color: 'white',
-                    padding: '15px',
-                    borderRadius: '5px',
-                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
-                    opacity: '0',
-                    transition: 'opacity 0.5s ease',
-                    fontSize: '20px',
-                    zIndex: '9999'
-                });
-
-                let countdown = 5;
-                notification.textContent = `Open document in ${countdown}...`;
-                document.body.appendChild(notification);
-                setTimeout(() => notification.style.opacity = '1', 10);
-
-                await new Promise((resolve) => {
-                    const interval = setInterval(() => {
-                        countdown--;
-                        notification.textContent = `Open document in ${countdown}...`;
-                        if (countdown <= 0) {
-                            clearInterval(interval);
-                            notification.style.opacity = '0';
-                            setTimeout(() => notification.remove(), 500);
-                            resolve();
-                        }
-                    }, 1000);
-                });
-
-                doc.scrollTo({
-                    top: currentPosition,
-                    behavior: "smooth"
-                });
-
-                genPDF();
-            }
-
-            countdownAndScroll(doc, currentPosition);
-        };
-
-        const checkContent = () => {
-            let a = [];
-            const pageContainer = document.getElementById("page-container").childNodes;
-            for (const e of pageContainer) {
-                if (e.childNodes[0].className === "page-content" && e.childNodes[0].childNodes.length !== 2) {
-                    a.push(e);
-                }
-            }
-            if (a.length === 0) return true;
-            return false;
         }
-
-        if (checkContent()) {
-            return genPDF();
-        } else {
-            if (!confirm(`Would you like me to scan this document for download?\n\n*Please configure Destination to "Save as PDF" to ensure the content is formatted correctly.`)) return;
-
-            try {
-                await loadContent();
-            } catch (error) {
-                alert("Failed to load content");
-            }
-        }
-    } else {
-        return alert("Please reload this page to clear all banners!");
     }
-}
+    // ----------------------------------------------------------
+
+    // ----------------------------------------------------------
+    // Countdown
+    // ----------------------------------------------------------
+    const countdownNotification = async (doc, message) => {
+        const notification = doc.createElement('div');
+
+        // Styling
+        Object.assign(notification.style, {
+            fontFamily: "sans-serif",
+            position: 'fixed',
+            top: '20px',
+            right: '20px',
+            backgroundColor: '#FF5722',
+            color: 'white',
+            padding: '15px',
+            borderRadius: '5px',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+            opacity: '0',
+            transition: 'opacity 0.5s ease',
+            fontSize: '20px',
+            zIndex: '9999'
+        });
+
+        let countdown = 5;
+        notification.textContent = `${message} in ${countdown}...`;
+
+        doc.body.appendChild(notification);
+        setTimeout(() => notification.style.opacity = '1', 10);
+
+        await new Promise((resolve) => {
+            const interval = setInterval(() => {
+                countdown--;
+                notification.textContent = `${message} in ${countdown}...`;
+                if (countdown <= 0) {
+                    clearInterval(interval);
+                    notification.remove();
+                    resolve();
+                }
+            }, 1000);
+        });
+    }
+    // ----------------------------------------------------------
+
+    // ----------------------------------------------------------
+    // Check document is loaded or not
+    const checkContent = () => {
+        let a = [];
+        const pageContainer = document.getElementById("page-container").childNodes;
+        for (const e of pageContainer) {
+            if (e.childNodes[0].className === "page-content" && e.childNodes[0].childNodes.length !== 2) {
+                a.push(e);
+            }
+        }
+        if (a.length === 0) return true;
+        return false;
+    }
+    // ----------------------------------------------------------
+
+    // ----------------------------------------------------------
+    // Scroll to specific position
+    // ----------------------------------------------------------
+    const scrollToPosition = async (position) => {
+        return new Promise((resolve) => {
+            const documentWrapper = document.getElementById("document-wrapper");
+
+            const onScroll = () => {
+                if (documentWrapper.scrollTop === position) {
+                    documentWrapper.removeEventListener("scroll", onScroll);
+
+                    resolve();
+                }
+            }
+
+            documentWrapper.scrollTo({
+                top: position,
+                behavior: "smooth"
+            });
+
+            documentWrapper.addEventListener("scroll", onScroll);
+        })
+    }
+    // ----------------------------------------------------------
+
+    // ----------------------------------------------------------
+    // Generate document
+    // ----------------------------------------------------------
+    const genDoc = async () => {
+        const docStyles = document.querySelector('link[rel="stylesheet"][href*="doc-assets"][href*=".studocu.com"]').cloneNode(true);
+
+        const newWindow = window.open("");
+        newWindow.document.title = document.title;
+        newWindow.document.head.appendChild(docStyles);
+        newWindow.document.head.appendChild(printConfig());
+        newWindow.document.body.appendChild(cloneDoc());
+
+        await countdownNotification(newWindow.document, "Download");
+        newWindow.print();
+    }
+    // ----------------------------------------------------------
+
+
+
+    // ----------------------------------------------------------
+    // Main processing
+    // ----------------------------------------------------------
+    // Valid this document is clean.
+    // ----------------------------------------------------------
+    if (!!document.querySelectorAll(`button[data-test-selector="preview-banner-upgrade-second-cta"]`).length) {
+        if (confirm(`Advertisement banners are detected in this document.\nClick "OK" to remove them and refresh the page.\nClick "Cancel" if you believe all banners have been removed.`)) {
+            location.reload();
+            return;
+        }
+    }
+
+    if (!checkContent()) {
+        // If document is not loaded yet
+        const currentPosition = getCurrentPosition();
+        await scanDoc();
+        await countdownNotification(document, "Open document");
+        await scrollToPosition(currentPosition);
+    }
+    return genDoc()
+    // ----------------------------------------------------------
+};
 
 document.getElementById("btn-download").addEventListener("click", async () => {
     try {
@@ -244,7 +255,7 @@ document.getElementById("btn-download").addEventListener("click", async () => {
 
         chrome.scripting.executeScript({
             target: { tabId: tab.id },
-            func: genDoc
+            func: main
         })
     } catch (error) {
         console.error(error);
